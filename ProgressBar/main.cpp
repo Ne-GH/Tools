40d87ad53bb729e7d024d7e87cc33dff76a7d990
Ne-GH <printfne@gmail.com>
2022-08-09 18:38:13 +0800

    updata mode file

--- ProgressBar/main.cpp
+++ ProgressBar/main.cpp
@@ -4,20 +4,32 @@
 */
 
 #include <iostream>
-#include "./ProgressBar.h"
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <Windows.h>
+// #include "./ProgressBar.h"
 
 using namespace std;
 
 int main(int argc,char *argv[]){
 
-    ProgressBar pb(100,10);
+int num = 0;
+    string str(101,'_');
+    str[0] = '[';
+    str[str.size()-1] = ']';
 
-    for(int i = 0;i < 10; ++i){
-        pb.Show();
-        pb._now ++;
+    int max = 100;
+    for(int i = 1;i <= 100; ++i){
+        
+        cout << left << setw(110) << i << endl;
+        cout << setw(103) << str << " " << i << "%" << "\r";
+        str[i] = '#';
+        // printf("%-100s %-2d\r",str,i);
+        Sleep(100);
     }
-
-
-
+    cout << endl;
+    cout << hex << 100 << endl;
+    cout << dec << 16 << endl;
     return 0;
 }
