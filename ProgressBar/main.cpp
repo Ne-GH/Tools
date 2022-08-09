a485c0414282b54070e12fcf34dcbe56cd64b9d8
Ne-GH <printfne@gmail.com>
2022-08-09 19:24:40 +0800

    updata color enmu

--- ProgressBar/main.cpp
+++ ProgressBar/main.cpp
@@ -7,29 +7,38 @@
 #include <string>
 #include <algorithm>
 #include <iomanip>
-#include <Windows.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+    #include <Windows.h>
+    #include "./windows_lib.h"
+#elif defined(__linux__) || defined(__unix__)
+    #include "./linux_lib.h"
+#endif
 // #include "./ProgressBar.h"
 
 using namespace std;
 
 int main(int argc,char *argv[]){
 
-int num = 0;
+    
+    int num = 0;
     string str(101,'_');
     str[0] = '[';
     str[str.size()-1] = ']';
 
     int max = 100;
     for(int i = 1;i <= 100; ++i){
-        
+        SetColor(i % 16);
         cout << left << setw(110) << i << endl;
         cout << setw(103) << str << " " << i << "%" << "\r";
         str[i] = '#';
         // printf("%-100s %-2d\r",str,i);
-        Sleep(100);
+        Sleep(50);
     }
     cout << endl;
-    cout << hex << 100 << endl;
-    cout << dec << 16 << endl;
+    SetColor(NONE);
+    cout << 100 ;
+
+    cin >> num;
     return 0;
 }
